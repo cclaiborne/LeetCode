cad4d2f67b2a56a17dbf4e23881a7b0b27c0d897
cclaiborne <claiborne206@gmail.com>
2015-08-03 18:08:16 -0700

    add Nine

---
+++ src/Nine.java
@@ -0,0 +1,18 @@
+public class Nine {
+	public static void main(String[] args) {
+		System.out.println(isPalindrome(-212));
+
+	}
+    public static boolean isPalindrome(int x) {
+    	boolean test = true;
+    	x = Math.abs(x);
+    	int length = String.valueOf(x).length();
+    	for(int i = 0; i<length/2; i++){
+    	if((int)(x/Math.pow(10, length-i-1))%10 == (int)(x / Math.pow(10, i))%10){
+    	}
+    	else
+    		return false;
+    	}
+    	return test;
+    }
+}
